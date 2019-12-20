5bda52127051641b4712147fae13d4b4654770e8
ileanahi <argyris.ileana@gmail.com>
2019-12-20 14:50:45 +0200

    Updates

---
+++ precache-manifest.343e79146bc0e40e85efec6bd68e0805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b005611648c460d92803c18dedc8265f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0d610615c33e8e6ca7c1",
+    "url": "/robofriends/static/css/main.90d05fa2.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/robofriends/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d610615c33e8e6ca7c1",
+    "url": "/robofriends/static/js/main.aaa8e001.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
