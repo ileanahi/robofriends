54c73eedec4cf8bf3b59ceacea2320f4b132ebcf
ileanahi <argyris.ileana@gmail.com>
2019-12-23 02:55:27 +0200

    Add Error Boundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({ robots }) => {
-    return (
-        <div>
-            {
-            robots.map((user, i) => {
-                return (
-                    <Card 
-                        key={robots[i].id} 
-                        id={robots[i].id} 
-                        name={robots[i].name} 
-                        email={robots[i].email} 
-                    />
-                );
-            })
+const CardList = ({ robots }) => (
+  <div>
+    {
+            robots.map((user, i) => (
+              <Card
+                key={robots[i].id}
+                id={robots[i].id}
+                name={robots[i].name}
+                email={robots[i].email}
+              />
+            ))
         }
-        </div>
-    );
-}
+  </div>
+);
 
 export default CardList;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends React.Component {
@@ -32,7 +33,9 @@ class App extends React.Component {
         <h1>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
        );
